04a1ec2610f878c7a182114628f88933e32f1ae7
Nda Youssef <140206711+NdaYoussef@users.noreply.github.com>
2024-09-10 14:44:29 +0300

    Add files via upload

---
+++ Market/Controllers/Auth/CategoryController.cs
@@ -0,0 +1,73 @@
+﻿using Market.Data;
+using Market.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly MarketContext context;
+        public CategoryController(MarketContext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public IActionResult GetCategory()
+        {
+            if (ModelState.IsValid)
+            {
+                List<Category> categories = context.Categories.ToList();
+                return Ok(categories);
+            }
+            return BadRequest();
+
+        }
+        [HttpPost]
+        public IActionResult AddCategory([FromBody] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+                return Ok(category);
+            }
+            return BadRequest();
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditCategory([FromRoute] int id, [FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var oldCategory = context.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (oldCategory == null)
+            {
+                return NotFound(); 
+            }
+            oldCategory.CategoryName = category.CategoryName;
+            context.SaveChanges();
+            return Ok(oldCategory);
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = context.Categories.FirstOrDefault(x => x.CategoryId == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ Market/Controllers/Auth/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using Market.Data;
+using Market.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly MarketContext context;
+        public ProductController(MarketContext _context)
+        {
+            context = _context;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            if (ModelState.IsValid)
+            {
+                List<Product> products = context.Products.ToList();
+                return Ok(products);
+            }
+            return BadRequest();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            Product product = context.Products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+            context.SaveChanges();
+        }
+
+        [HttpPost]
+        public IActionResult PostProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                 context.Products.Add(product);
+                context.SaveChanges();
+                return Ok(new { message = "new Product has been added successfully" });
+            }
+            return BadRequest();
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            Product OldProduct = context.Products.FirstOrDefault(x => x.Id == id);
+            if (ModelState.IsValid)
+            {
+                if (OldProduct == null)
+                {
+                    return NotFound();
+                }
+                product.Name = OldProduct.Name;
+                product.Description = OldProduct.Description;
+                product.Price = OldProduct.Price;
+            }
+            return StatusCode(StatusCodes.Status204NoContent);
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            Product product = context.Products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return BadRequest();
+        }
+    }
+}

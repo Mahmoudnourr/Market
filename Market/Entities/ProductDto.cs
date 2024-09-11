695deebaa6764594298c03eebb3d33b99fa638a3
Nda Youssef <140206711+NdaYoussef@users.noreply.github.com>
2024-09-11 18:04:03 +0300

    Add files via upload

---
+++ Market/Entities/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Market.Entities
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name cannot be over 50 character")]
+        public string Name { get; set; }
+
+        [StringLength(200, ErrorMessage = "Description cannot be over 200 character !!")]
+        public string? Description { get; set; }
+        public float Price { get; set; }
+        [Required(ErrorMessage = "Category is required.")]
+        public int CategoryId { get; set; }
+
+    }
+    
+}
